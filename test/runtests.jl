54bf348b5be3f6e97cd4c590dab8ce50536a567b
MichaelHatherly <michaelhatherly@gmail.com>
2024-01-08 15:28:51 +0000

    Fix CI issues on macOS 1.6
    The worker processes running the notebooks appear to stall on macOS 1.6 when
    precompiling some of the more costly notebook project environments. Avoid that
    by running the precompilation separately prior to starting the tests.
    
    Adds the cache action to try to save as much of that precompilation work as
    possible.

--- test/runtests.jl
+++ test/runtests.jl
@@ -25,6 +25,25 @@ function with_extension(path, ext)
     return "$root.$ext"
 end
 
+# Update and precompile all project TOMLs when in CI.
+if get(ENV, "CI", "false") == "true"
+    for dir in ["integrations", "mimetypes"]
+        for (root, dirs, files) in walkdir(joinpath(@__DIR__, "examples", dir))
+            for each in files
+                if each == "Project.toml"
+                    manifest = joinpath(root, "Manifest.toml")
+                    if isfile(manifest)
+                        rm(manifest; force = true)
+                    end
+                    run(
+                        `$(Base.julia_cmd()) --project=$root -e 'push!(LOAD_PATH, "@stdlib"); import Pkg; Pkg.update()'`,
+                    )
+                end
+            end
+        end
+    end
+end
+
 @testset "QuartoNotebookRunner" begin
     @testset "socket server" begin
         cd(@__DIR__) do
