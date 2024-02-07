bf437df5e63102d3651b9a167e4c293e67e711a3
MichaelHatherly <michaelhatherly@gmail.com>
2024-02-07 13:17:02 +0000

    Add a comment about the env vars

--- test/utilities/project_precompile.jl
+++ test/utilities/project_precompile.jl
@@ -1,5 +1,6 @@
 # Update and precompile all project TOMLs when in CI.
 if get(ENV, "CI", "false") == "true"
+    # To avoid warnings related to GKS during CI runs on Linux with Plots.jl GR backend.
     if Sys.islinux()
         ENV["GKS_ENCODING"] = "utf8"
         ENV["GKSwstype"] = "nul"
