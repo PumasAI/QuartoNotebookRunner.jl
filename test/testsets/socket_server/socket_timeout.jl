f17fefbcf4f5c431eeadb5ac952cdf3d44cc7d76
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2024-03-13 16:02:07 +0100

    do second connection via node

--- test/testsets/socket_server/socket_timeout.jl
+++ test/testsets/socket_server/socket_timeout.jl
@@ -1,8 +1,12 @@
-include("../utilities/prelude.jl")
+include("../../utilities/prelude.jl")
 
 using Sockets
 
 @testset "Socket timeout" begin
+    node = NodeJS_18_jll.node()
+    client = joinpath(@__DIR__, "client.js")
+    json(cmd) = JSON3.read(read(cmd, String), Any)
+
     server = QuartoNotebookRunner.serve(; timeout = 1)
     sock = Sockets.connect(server.port)
 
@@ -15,17 +19,14 @@ using Sockets
         sock,
         Dict(
             :type => "run",
-            :content => abspath(joinpath(@__DIR__, "..", "examples", "sleep_3.qmd")),
+            :content => abspath(joinpath(@__DIR__, "..", "..", "examples", "sleep_3.qmd")),
         ),
     )
     println(sock)
 
     # check that the server stays alive during a 3 second long command, even if in the meantime
     # shorter-lived commands are completed and the server timeout is only 1 second
-    sock_2 = Sockets.connect(server.port)
-    JSON3.write(sock_2, Dict(:type => "isready", :content => Dict()))
-    println(sock_2)
-    @test readline(sock_2) == "true"
+    @test json(`$node $client $(server.port) isready`)
 
     # now we wait for the longer command to complete
     @test !isempty(readline(sock))
