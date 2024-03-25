c89732b49b0c04f60652cd5cac7b53e15e7087db
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2024-03-25 11:32:51 +0100

    add tests

--- test/utilities/cleanup.jl
+++ test/utilities/cleanup.jl
@@ -1,5 +1,7 @@
 let removed = []
     for (root, dirs, files) in walkdir(joinpath(@__DIR__, ".."); topdown = false)
+        abspath(root) == abspath(joinpath(@__DIR__, "..", "assets")) && continue
+        
         for file in files
             _, ext = splitext(file)
             if ext in (".html", ".pdf", ".tex", ".docx", ".typ", ".ipynb", ".png", ".css")
