96295efb592bbde2e57587369c632fa62ee8f7e9
MichaelHatherly <michaelhatherly@gmail.com>
2024-04-12 15:05:00 +0100

    `RCall.jl` plot support
    Fixes #92.

---
+++ test/testsets/integrations/RCall.jl
@@ -0,0 +1,12 @@
+include("../../utilities/prelude.jl")
+
+test_example(joinpath(@__DIR__, "../../examples/integrations/RCall.qmd")) do json
+    cells = json["cells"]
+    cell = cells[4]
+    output = cell["outputs"][1]
+
+    @test !isempty(output["data"]["image/png"])
+
+    @test cell["outputs"][1]["metadata"]["image/png"] ==
+          Dict("width" => 600, "height" => 450)
+end
