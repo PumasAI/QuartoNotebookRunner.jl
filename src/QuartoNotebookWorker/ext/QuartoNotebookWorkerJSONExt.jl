503a6678901a01b8171b1c430a30d24ba154014e
MichaelHatherly <michaelhatherly@gmail.com>
2024-05-10 16:51:57 +0100

    Convert `ojs_define` into a package extension

---
+++ src/QuartoNotebookWorker/ext/QuartoNotebookWorkerJSONExt.jl
@@ -0,0 +1,12 @@
+module QuartoNotebookWorkerJSONExt
+
+import QuartoNotebookWorker
+import JSON
+
+function QuartoNotebookWorker._ojs_define(::QuartoNotebookWorker.OJSDefine, kwargs)
+    contents = QuartoNotebookWorker.ojs_convert(kwargs)
+    json = JSON.json(Dict("contents" => contents))
+    return HTML("<script type='ojs-define'>$(json)</script>")
+end
+
+end
--- src/QuartoNotebookWorker/src/ojs_define.jl
+++ src/QuartoNotebookWorker/src/ojs_define.jl
@@ -1,27 +1,11 @@
-# TODO: move to a package extension.
+struct OJSDefine end
 
-function ojs_define(; kwargs...)
-    json_id = Base.PkgId(Base.UUID("682c06a0-de6a-54ab-a142-c8b1cf79cde6"), "JSON")
-    dataframes_id =
-        Base.PkgId(Base.UUID("a93c6f00-e57d-5684-b7b6-d8193f3e46c0"), "DataFrames")
-    tables_id = Base.PkgId(Base.UUID("5d742f6a-9f54-50ce-8119-136d35baa42b"), "Tables")
+ojs_define(; kwargs...) = _ojs_define(OJSDefine(), kwargs)
 
-    if haskey(Base.loaded_modules, json_id)
-        JSON = Base.loaded_modules[json_id]
-        contents =
-            if haskey(Base.loaded_modules, dataframes_id) &&
-               haskey(Base.loaded_modules, tables_id)
-                DataFrames = Base.loaded_modules[dataframes_id]
-                Tables = Base.loaded_modules[tables_id]
-                conv(x) = isa(x, DataFrames.AbstractDataFrame) ? Tables.rows(x) : x
-                [Dict("name" => k, "value" => conv(v)) for (k, v) in kwargs]
-            else
-                [Dict("name" => k, "value" => v) for (k, v) in kwargs]
-            end
-        json = JSON.json(Dict("contents" => contents))
-        return HTML("<script type='ojs-define'>$(json)</script>")
-    else
-        @warn "JSON package not available. Please install the JSON.jl package to use ojs_define."
-        return nothing
-    end
+function _ojs_define(::Any, kwargs)
+    @warn "JSON package not available. Please install the JSON.jl package to use ojs_define."
+    return nothing
 end
+
+ojs_convert(kwargs) = [Dict("name" => k, "value" => _ojs_convert(v)) for (k, v) in kwargs]
+_ojs_convert(x) = x
