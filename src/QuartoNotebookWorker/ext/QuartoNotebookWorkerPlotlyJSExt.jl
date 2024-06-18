0d5c13ffc7990abf75b1ff3bd8784253ae0ae7e2
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2024-06-18 15:39:44 +0200

    change forwarding mechanism to `expand`

--- src/QuartoNotebookWorker/ext/QuartoNotebookWorkerPlotlyJSExt.jl
+++ src/QuartoNotebookWorker/ext/QuartoNotebookWorkerPlotlyJSExt.jl
@@ -3,6 +3,6 @@ module QuartoNotebookWorkerPlotlyJSExt
 import QuartoNotebookWorker
 import PlotlyJS
 
-QuartoNotebookWorker._mimetype_wrapper(p::PlotlyJS.SyncPlot) = QuartoNotebookWorker._mimetype_wrapper(p.plot)
+QuartoNotebookWorker.expand(p::PlotlyJS.SyncPlot) = QuartoNotebookWorker.expand(p.plot)
 
 end
