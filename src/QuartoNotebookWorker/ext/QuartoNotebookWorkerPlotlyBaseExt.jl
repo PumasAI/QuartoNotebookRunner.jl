bb622b0dc281af7185c48c4a3ace537fe6b7229c
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2024-06-18 15:34:20 +0200

    add mechanism to insert require.js preamble into its own cell before first plotly plot

--- src/QuartoNotebookWorker/ext/QuartoNotebookWorkerPlotlyBaseExt.jl
+++ src/QuartoNotebookWorker/ext/QuartoNotebookWorkerPlotlyBaseExt.jl
@@ -4,16 +4,59 @@ import QuartoNotebookWorker
 import PlotlyBase
 
 QuartoNotebookWorker._mimetype_wrapper(p::PlotlyBase.Plot) = PlotlyBasePlot(p)
-
 struct PlotlyBasePlot <: QuartoNotebookWorker.WrapperType
     value::PlotlyBase.Plot
 end
 
-function Base.show(io::IO, ::MIME"text/html", wrapper::PlotlyBasePlot)
+struct PlotlyBasePlotWithoutRequireJS
+    plot::PlotlyBase.Plot
+end
+
+struct PlotlyRequireJSConfig end
+
+const FIRST_PLOT_DISPLAYED = Ref(false)
+
+function QuartoNotebookWorker.expand(p::PlotlyBasePlot)
+
+    plotcell = QuartoNotebookWorker.Cell(PlotlyBasePlotWithoutRequireJS(p.value))
+
+    # Quarto expects that the require.js preamble which Plotly needs to function
+    # comes in its own cell, which will then be hoisted into the HTML page header.
+    # So we cannot have that preamble concatenated with every plot's HTML content.
+    # Instead, e keep track whether a Plotly plot is the first per notebook, and in that
+    # case have it expand into the preamble cell and the plot cell. If it's not the
+    # first time, we expand only into the plot cell.
+    cells = if !FIRST_PLOT_DISPLAYED[]
+        [QuartoNotebookWorker.Cell(PlotlyRequireJSConfig()), plotcell]
+    else
+        [plotcell]
+    end
+
+    FIRST_PLOT_DISPLAYED[] = true
+
+    return cells
+end
+
+function Base.show(io::IO, ::MIME"text/html", p::PlotlyBasePlotWithoutRequireJS)
     # We want to embed only the minimum markup needed to render the
     # plotlyjs plots, otherwise a full HTML page is generated for every
     # plot which does not render correctly in our context.
-    PlotlyBase.to_html(io, wrapper.value; include_plotlyjs = "require-loaded", full_html = false)
+    # "require-loaded" means that we pass the require.js preamble ourselves.
+    PlotlyBase.to_html(io, p.plot; include_plotlyjs = "require-loaded", full_html = false)
+end
+
+function Base.show(io::IO, ::MIME"text/html", ::PlotlyRequireJSConfig)
+    print(io, PlotlyBase._requirejs_config())
+end
+
+function reset_first_plot_displayed_flag!()
+    FIRST_PLOT_DISPLAYED[] = false
+end
+
+function __init__()
+    if ccall(:jl_generating_output, Cint, ()) == 0
+        QuartoNotebookWorker.add_package_refresh_hook!(reset_first_plot_displayed_flag!)
+    end
 end
 
 end
