ba525c5e908e96beefcbbe634d02dad45485c8b3
Michael Hatherly <michaelhatherly@gmail.com>
2024-10-18 09:57:54 +0100

    Revert "Pin `JuliaFormatter` to `v2` and fix new formatting failures (#199)" (#201)
    This reverts commit 328915b11baab30564f2afcc94a15239e427f8e0.

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,7 +1,7 @@
 PrecompileTools.@setup_workload begin
     notebook = joinpath(@__DIR__, "..", "test", "examples", "cell_types.qmd")
     script = joinpath(@__DIR__, "..", "test", "examples", "cell_types.jl")
-    results = Dict{String,@NamedTuple{error::Bool,data::Vector{UInt8}}}()
+    results = Dict{String,@NamedTuple{error::Bool, data::Vector{UInt8}}}()
     PrecompileTools.@compile_workload begin
         raw_text_chunks(notebook)
         raw_text_chunks(script)
