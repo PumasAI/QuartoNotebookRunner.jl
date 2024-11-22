dd91dbce0979e45fc30b4c8d4824bb61b214bd20
MichaelHatherly <michaelhatherly@gmail.com>
2024-11-22 15:47:18 +0000

    Add missing `Random` import

--- test/testsets/integrations/RCall.jl
+++ test/testsets/integrations/RCall.jl
@@ -10,3 +10,12 @@ test_example(joinpath(@__DIR__, "../../examples/integrations/RCall.qmd")) do jso
     @test cell["outputs"][1]["metadata"]["image/png"] ==
           Dict("width" => 600, "height" => 450)
 end
+
+test_example(joinpath(@__DIR__, "../../examples/integrations/RCallSVG.qmd")) do json
+    cells = json["cells"]
+    cell = cells[4]
+    output = cell["outputs"][1]
+
+    @test !isempty(output["data"]["image/svg+xml"])
+    @test isempty(cell["outputs"][1]["metadata"])
+end
