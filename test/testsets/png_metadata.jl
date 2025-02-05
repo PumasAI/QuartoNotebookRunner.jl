208f68eb60d4bb58f560232386fdb600f8ac2826
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2025-02-05 13:38:44 +0100

    add tests

--- test/testsets/png_metadata.jl
+++ test/testsets/png_metadata.jl
@@ -7,4 +7,16 @@ include("../utilities/prelude.jl")
     @test QuartoNotebookRunner.png_image_metadata(
         read(joinpath(@__DIR__, "..", "assets", "15x10.png")),
     ) == (; width = 15, height = 10)
+    @test QuartoNotebookRunner.png_image_metadata(
+        read(joinpath(@__DIR__, "..", "assets", "black_no_dpi.png")),
+    ) == (; width = 100, height = 100)
+    @test QuartoNotebookRunner.png_image_metadata(
+        read(joinpath(@__DIR__, "..", "assets", "black_96_dpi.png")),
+    ) == (; width = 100, height = 100)
+    @test QuartoNotebookRunner.png_image_metadata(
+        read(joinpath(@__DIR__, "..", "assets", "black_300_dpi.png")),
+    ) == (; width = 32, height = 32)
+    @test QuartoNotebookRunner.png_image_metadata(
+        read(joinpath(@__DIR__, "..", "assets", "black_600_dpi.png")),
+    ) == (; width = 16, height = 16)
 end
