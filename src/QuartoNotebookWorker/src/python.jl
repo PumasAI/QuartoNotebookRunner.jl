2542d5476b6d943f24e392d40ad22a65806e2946
MichaelHatherly <michaelhatherly@gmail.com>
2025-02-18 06:39:13 +0000

    Ensure Python stacktraces list correct files+lines, add `?` mode

--- src/QuartoNotebookWorker/src/python.jl
+++ src/QuartoNotebookWorker/src/python.jl
@@ -1,5 +1,7 @@
 macro py_str(str)
-    return esc(py_expr(str))
+    return esc(py_expr(str, __source__, __module__))
 end
-py_expr(code::AbstractString) = _py_expr(nothing, code)
-_py_expr(::Any, code::AbstractString) = :(error("`PythonCall.jl` package not loaded."))
+py_expr(code::AbstractString, src::LineNumberNode, mod::Module) =
+    _py_expr(nothing, code, src, mod)
+_py_expr(::Any, code::AbstractString, src, mod) =
+    :(error("`PythonCall.jl` package not loaded."))
