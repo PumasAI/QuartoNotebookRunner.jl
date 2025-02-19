914370583927a5ae1ab9ffc8e41b9b5041a82b92
MichaelHatherly <michaelhatherly@gmail.com>
2025-02-19 07:18:14 +0000

    Clean up `WorkerSetup` module

--- src/WorkerSetup.jl
+++ src/WorkerSetup.jl
@@ -16,39 +16,10 @@ module WorkerSetup
 
 # Imports.
 
-import Pkg
 import RelocatableFolders
-import Scratch
-import TOML
 
-# Package init-time.
-
-# To allow it to be added to a system image we make sure it is relocatable.
+# Support adding to a system image.
 const QNW = RelocatableFolders.@path joinpath(@__DIR__, "QuartoNotebookWorker")
-let
-    # Any content from the worker package should trigger recompilation in the
-    # runner package, for ease of development.
-    for (root, dirs, files) in walkdir(QNW)
-        for file in files
-            include_dependency(joinpath(root, file))
-        end
-    end
-end
-
-# The loader environment is used to load the worker package into whatever
-# environment that the user has started the process with.
-const LOADER_ENV = Ref("")
-
-# Since we start a task to perform the loader env setup at package init-time we
-# don't want that to block `using QuartoNotebookRunner` so we lock the setup
-# task to allow prevention of starting the loader env until
-const WORKER_SETUP_LOCK = ReentrantLock()
-
-function __init__()
-    if ccall(:jl_generating_output, Cint, ()) == 0
-        LOADER_ENV[] = String(QNW)
-    end
-end
 
 # Debugging utilities.
 
@@ -69,53 +40,14 @@ julia> include("runtests.jl")
 to run the test suite without having to reload the worker package.
 """
 function debug(; exeflags = String[])
-    if islocked(WORKER_SETUP_LOCK)
-        error("Worker setup is in progress. Please try again later.")
-    else
-        mktempdir() do temp_dir
-            file = joinpath(temp_dir, "setup.jl")
-            project = LOADER_ENV[]
-            write(
-                file,
-                """
-                # Try load `Revise` first, since we want to be able to track
-                # changes in the worker package.
-                try
-                    import Revise
-                catch error
-                    @info "Revise not available."
-                end
-
-                cd($(repr(QNW)))
-
-                pushfirst!(LOAD_PATH, $(repr(project)))
-
-                # Always do a `precompile` so that it's simpler to kill and
-                # restart the worker without it potentially being stale.
-                import Pkg
-                Pkg.precompile()
-
-                import QuartoNotebookWorker
-
-                # Attempt to import some other useful packages.
-                try
-                    import Debugger
-                catch error
-                    @info "Debugger not available."
-                end
-                try
-                    import TestEnv
-                catch error
-                    @info "TestEnv not available."
-                end
-                """,
-            )
-            julia = Base.julia_cmd()[1]
-            cmd = `$julia $exeflags --startup-file=no -i $file`
-            run(cmd)
-        end
+    mktempdir() do temp_dir
+        julia = Base.julia_cmd()[1]
+        debug_env = joinpath(temp_dir, "QuartoNotebookWorker.DEBUG")
+        cmd = `$julia $exeflags --project=$debug_env --startup-file=no -i $DEBUG_STARTUP`
+        run(addenv(cmd, "QUARTONOTEBOOKWORKER_PACKAGE" => String(QNW)))
     end
 end
+const DEBUG_STARTUP = RelocatableFolders.@path joinpath(@__DIR__, "debug_startup.jl")
 
 """
     test()
@@ -126,25 +58,12 @@ the `TestEnv` package to do so.
 """
 function test(; exeflags = String[])
     mktempdir() do temp_dir
-        file = joinpath(temp_dir, "runtests.jl")
-        project = LOADER_ENV[]
-        write(
-            file,
-            """
-            pushfirst!(LOAD_PATH, "@stdlib")
-            import Pkg
-            popfirst!(LOAD_PATH)
-
-            cd($(repr(QNW)))
-
-            pushfirst!(LOAD_PATH, $(repr(project)))
-            Pkg.test("QuartoNotebookWorker")
-            """,
-        )
         julia = Base.julia_cmd()[1]
-        cmd = `$julia $exeflags --startup-file=no $file`
-        run(cmd)
+        test_env = joinpath(temp_dir, "QuartoNotebookWorker.TEST")
+        cmd = `$julia $exeflags --project=$test_env --startup-file=no $TEST_STARTUP`
+        run(addenv(cmd, "QUARTONOTEBOOKWORKER_PACKAGE" => String(QNW)))
     end
 end
+const TEST_STARTUP = RelocatableFolders.@path joinpath(@__DIR__, "test_startup.jl")
 
 end
