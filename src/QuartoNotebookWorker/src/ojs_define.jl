97a17213e4c08925b9ff13b960c34a79d68b1dec
Alexey Stukalov <astukalol@seer.bio>
2025-03-22 12:00:30 -0700

    ojs_define()/ojs_convert(): docstrings

--- src/QuartoNotebookWorker/src/ojs_define.jl
+++ src/QuartoNotebookWorker/src/ojs_define.jl
@@ -1,12 +1,29 @@
 struct OJSDefine end
 
+"""
+    ojs_define(; kwargs...)
+
+User-facing function that, given a list of `name = object` pairs,
+makes them available to *ObservableJS* by rendering their
+HTML/JavaScript representation.
+"""
 ojs_define(; kwargs...) = _ojs_define(OJSDefine(), kwargs)
 
+# Internal function that implements rendering of the `name = object` pairs.
+# The actual implementation is in the QuartoNotebookWorkerJSONExt extension.
 function _ojs_define(::Any, kwargs)
     @warn "JSON package not available. Please install the JSON.jl package to use ojs_define."
     return nothing
 end
 
+"""
+    ojs_convert(kwargs)
+
+Given an iterator of `name => object` pairs, return a vector of
+`Dict("name" => name, "value" => lowered_object)`, where the `lowered_object`
+(the result of `_ojs_convert(object)`) is the `object` representation
+that supports direct conversion to JSON.
+"""
 ojs_convert(kwargs) = [Dict("name" => k, "value" => _ojs_convert(v)) for (k, v) in kwargs]
 
 # Internal function to check if the object supports a specific trait.
