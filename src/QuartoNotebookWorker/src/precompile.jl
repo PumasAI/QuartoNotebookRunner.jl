e30c5e54965219c3c0a267d276673f2bd0f61d55
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2025-09-29 14:38:42 +0200

    kwcall is a 1.9 method

--- src/QuartoNotebookWorker/src/precompile.jl
+++ src/QuartoNotebookWorker/src/precompile.jl
@@ -1,22 +1,24 @@
 precompile(Tuple{typeof(QuartoNotebookWorker.Malt.main)})
 precompile(Tuple{typeof(QuartoNotebookWorker.Malt._bson_deserialize), QuartoNotebookWorker.Malt.Sockets.TCPSocket})
 
-for type in [Int,Float64,String,Nothing,Missing]
-    precompile(
-        Tuple{
-            typeof(Core.kwcall),
-            NamedTuple{(:inline,), Tuple{Bool}},
-            typeof(QuartoNotebookWorker.render_mimetypes),
-            type,
-            Base.Dict{String, Any}
-        }
-    )
+if VERSION >= v"1.9"
+    for type in [Int,Float64,String,Nothing,Missing]
+        precompile(
+            Tuple{
+                typeof(Core.kwcall),
+                NamedTuple{(:inline,), Tuple{Bool}},
+                typeof(QuartoNotebookWorker.render_mimetypes),
+                type,
+                Base.Dict{String, Any}
+            }
+        )
+    end
+    precompile(Tuple{typeof(Core.kwcall), NamedTuple{(:file, :line, :cell_options), Tuple{String, Int64, Base.Dict{String, Any}}}, typeof(QuartoNotebookWorker.include_str), Module, String})
 end
 
 precompile(Tuple{typeof(Base.Filesystem.mkpath), String})
 precompile(Tuple{typeof(QuartoNotebookWorker.refresh!), Base.Dict{String, Any}})
 precompile(Tuple{typeof(QuartoNotebookWorker.refresh!), Base.Dict{String, Any}, Base.Dict{String, Any}})
-precompile(Tuple{typeof(Core.kwcall), NamedTuple{(:file, :line, :cell_options), Tuple{String, Int64, Base.Dict{String, Any}}}, typeof(QuartoNotebookWorker.include_str), Module, String})
 
 module __PrecompilationModule end
 
